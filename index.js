b1230e002aacc16ed23a455d6f0a4da27f8c7349
purerosefallen <78877@qq.com>
2019-08-25 17:50:09 +0800

    fix

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ ygopro.stoc_follow_after("CHANGE_SIDE", true, (buffer, info, client, server, dat
 	if (!room) {
 		return false;
 	}
-	ygopro.ctos_send(server, {
+	ygopro.ctos_send(server, "UPDATE_DECK", {
 		mainc: client.main.length,
 		sidec: client.side.length,
 		deckbuf: client.main.concat(client.side)
